af2712df0dbb89ce62d399ddb74f2202059432e7
Brandon Avant <avant.brandon@gmail.com>
2021-03-21 03:29:54 -0500

    Added Participant creation test.

--- src/HuntBot.Tests/UnitTests/Domain/HuntBotGameTests.cs
+++ src/HuntBot.Tests/UnitTests/Domain/HuntBotGameTests.cs
@@ -1,4 +1,6 @@
 ﻿using HuntBot.Domain.HuntBotGames;
+using HuntBot.Domain.HuntBotGames.Participants;
+using HuntBot.Domain.HuntBotGames.Rules;
 using HuntBot.Domain.SeedWork;
 using Moq;
 using System;
@@ -14,47 +16,63 @@ namespace HuntBot.Tests.UnitTests.Domain
         /// <summary>
         /// Default start date, which can be used when creating a new <see cref="HuntBotGame"/>, if no uniqueness is needed for the <see cref="DateTime"/> value.
         /// </summary>
-        private readonly DateTime _defaultStartDate;
+        private readonly DateTime _defaultGameStartDate;
 
         /// <summary>
         /// Default end date, which can be used when creating a new <see cref="HuntBotGame"/>, if no uniqueness is needed for the <see cref="DateTime"/> value.
         /// </summary>
-        private readonly DateTime _defaultEndDate;
+        private readonly DateTime _defaultGameEndDate;
+
+        /// <summary>
+        /// Default game title, which can be used when creating a <see cref="HuntBotGame"/> instance if no uniqueness is needed for the title.
+        /// </summary>
+        private readonly string _defaultGameTitle;
 
         /// <summary>
         /// Initializes default values for <see cref="HuntBotGame"/> unit tests.
         /// </summary>
         public HuntBotGameTests()
         {
-            _defaultStartDate = DateTime.UtcNow.AddHours(1);
-            _defaultEndDate = _defaultStartDate.AddDays(1);
+            _defaultGameStartDate = DateTime.UtcNow.AddHours(1);
+            _defaultGameEndDate = _defaultGameStartDate.AddDays(1);
+            _defaultGameTitle = "DefaultGameTitle";
         }
 
-        /// <summary>
-        /// Test that ensures that when invoking <see cref="HuntBotGame.CreateNewHuntBotGame"/> with valid values, the new instance's properties match what is expected.
-        /// </summary>
         [Fact]
         public void HuntBotGame_CreateNewHuntBotGameWithValidParams_CreatesInstanceWithCorrectValues()
         {
             HuntBotGame huntBotGame;
 
             var id = Guid.NewGuid();
-            var title = "MyGameTitle";
             var gameUniquenessCheckerMock = new Mock<IGameUniquenessChecker>();
 
-            gameUniquenessCheckerMock.Setup(uc => uc.IsUnique(title)).Returns(true);
-            huntBotGame = HuntBotGame.CreateNewHuntBotGame(id, title, _defaultStartDate, _defaultEndDate, gameUniquenessCheckerMock.Object);
+            gameUniquenessCheckerMock.Setup(uc => uc.IsUnique(_defaultGameTitle)).Returns(true);
+            huntBotGame = HuntBotGame.CreateNewHuntBotGame(id, _defaultGameTitle, _defaultGameStartDate, _defaultGameEndDate, gameUniquenessCheckerMock.Object);
 
             Assert.NotNull(huntBotGame);
             Assert.Equal(id, huntBotGame.Id);
-            Assert.Equal(title, huntBotGame.Title);
-            Assert.Equal(_defaultStartDate, huntBotGame.StartDate);
-            Assert.Equal(_defaultEndDate, huntBotGame.EndDate);
+            Assert.Equal(_defaultGameTitle, huntBotGame.Title);
+            Assert.Equal(_defaultGameStartDate, huntBotGame.StartDate);
+            Assert.Equal(_defaultGameEndDate, huntBotGame.EndDate);
         }
 
-        /// <summary>
-        /// Test that ensures that when invoking <see cref="HuntBotGame.AddParticipant"/> with an invalid title length, <see cref="BusinessRuleValidationException"/> is thrown.
-        /// </summary>
+        [Fact]
+        public void HuntBotGame_CreateNewHuntBotGameWithNonUniqueTitle_ThrowsBusinessRuleValidationException()
+        {
+            var id = Guid.NewGuid();
+            var gameUniquenessCheckerMock = new Mock<IGameUniquenessChecker>();
+
+            gameUniquenessCheckerMock.Setup(uc => uc.IsUnique(_defaultGameTitle)).Returns(false);
+
+            Assert.Throws<BusinessRuleValidationException>(() => HuntBotGame.CreateNewHuntBotGame(
+                id,
+                _defaultGameTitle,
+                _defaultGameStartDate,
+                _defaultGameEndDate,
+                gameUniquenessCheckerMock.Object
+            ));
+        }
+
         [Fact]
         public void HuntBotGame_CreateNewHuntBotGameWithInvalidTitleLength_ThrowsBusinessRuleValidationException()
         {
@@ -65,10 +83,67 @@ namespace HuntBot.Tests.UnitTests.Domain
             Assert.Throws<BusinessRuleValidationException>(() => HuntBotGame.CreateNewHuntBotGame(
               id,
               title,
-              _defaultStartDate,
-              _defaultEndDate,
+              _defaultGameStartDate,
+              _defaultGameEndDate,
               gameUniquenessCheckerMock.Object
             ));
+        }
+
+        [Fact]
+        public void HuntBotGame_CreateNewHuntBotGameWithStartDateInThePast_ThrowsBusinessRuleValidationException()
+        {
+            var id = Guid.NewGuid();
+            var yesterday = DateTime.UtcNow.AddDays(-1);
+            var gameUniquenessCheckerMock = new Mock<IGameUniquenessChecker>();
+
+            Assert.Throws<BusinessRuleValidationException>(() => HuntBotGame.CreateNewHuntBotGame(
+                id,
+                _defaultGameTitle,
+                yesterday,
+                _defaultGameEndDate,
+                gameUniquenessCheckerMock.Object
+            ));
+        }
+
+        [Fact]
+        public void HuntBotGame_CreateNewHuntBotGameWithEndDateMatchingStartDate_ThrowsBusinessRuleValidationException()
+        {
+            var id = Guid.NewGuid();
+            var gameUniquenessCheckerMock = new Mock<IGameUniquenessChecker>();
+
+            Assert.Throws<BusinessRuleValidationException>(() => HuntBotGame.CreateNewHuntBotGame(
+                id,
+                _defaultGameTitle,
+                _defaultGameStartDate,
+                _defaultGameStartDate,
+                gameUniquenessCheckerMock.Object
+            ));
+        }
+
+        [Fact]
+        public void HuntBotGame_AddParticipantWithValidParams_CreatesInstanceWithCorrectValues()
+        {
+            HuntBotGameParticipant huntBotGameParticipant;
+            HuntBotGameParticipant huntBotGameParticipantInList;
+            HuntBotGame huntBotGame;
+
+            var huntBotGameId = Guid.NewGuid();
+            var gameUniquenessCheckerMock = new Mock<IGameUniquenessChecker>();
+            var citizenNumber = 339566;
+            var citizenName = "Droog";
+            var participantUniquenessCheckerMock = new Mock<IParticipantUniquenessChecker>();
+
+            gameUniquenessCheckerMock.Setup(uc => uc.IsUnique(_defaultGameTitle)).Returns(true);
+            participantUniquenessCheckerMock.Setup(uc => uc.IsUnique(huntBotGameId, citizenNumber)).Returns(true);
+
+            huntBotGame = HuntBotGame.CreateNewHuntBotGame(huntBotGameId, _defaultGameTitle, _defaultGameStartDate, _defaultGameEndDate, gameUniquenessCheckerMock.Object);
+            huntBotGameParticipant = huntBotGame.AddParticipant(citizenNumber, citizenName, participantUniquenessCheckerMock.Object);
+            huntBotGameParticipantInList = huntBotGame.Participants[0];
+
+            Assert.Single(huntBotGame.Participants);
+            Assert.Equal(huntBotGameParticipant, huntBotGameParticipantInList);
+            Assert.Equal(citizenNumber, huntBotGameParticipantInList.Id);
+            Assert.Equal(citizenName, huntBotGameParticipantInList.CitizenName);
         }
 
         //public void HuntBotGame_AddParticipant
