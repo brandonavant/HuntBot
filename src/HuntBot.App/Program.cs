89f9911da7d27ea0a7c7cda42a4d16ebac25adb7
Brandon Avant <avant.brandon@gmail.com>
2021-04-11 15:26:52 -0500

    Added database init functionality

--- src/HuntBot.App/Program.cs
+++ src/HuntBot.App/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
+using HuntBot.Infrastructure;
+using HuntBot.Infrastructure.Database.Sqlite;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -7,14 +10,14 @@ using Microsoft.Extensions.Hosting;
 namespace HuntBot.App
 {
     class Program
-    {
+    {    
         /// <summary>
         /// Entrypoint of the .NET application.
         /// </summary>
         /// <param name="args">Arguments passed into the application via command-line.</param>
         public static async Task Main(string[] args)
         {
-            using IHost host = CreateHostBuilder(args).Build();
+            using IHost host = CreateHostBuilder(args).Build();            
 
             try
             {
@@ -33,7 +36,7 @@ namespace HuntBot.App
             // you would want to utilize an appsettings.Local.json, which would NOT be checked
             // into Git. This allows you to feed AW Configuration values without fear of secrets
             // ending up in Git.
-            var environmentName = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT");
+            var environmentName = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT");            
 
             return Host.CreateDefaultBuilder(args)
                 .ConfigureHostConfiguration(configHost =>
@@ -45,6 +48,7 @@ namespace HuntBot.App
                 .ConfigureServices((hostContext, services) =>
                 {
                     services.AddHostedService<HuntBot>();
+                    services.AddInfrastructureDependencies();
                 });
         }
     }
