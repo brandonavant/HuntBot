db838bd64d362c19dc9254fb70e2fd38ef7aafa8
Brandon Avant <avant.brandon@gmail.com>
2021-04-17 02:30:00 -0500

    Added XML documentation for SqliteAggregateStore

--- src/HuntBot.Infrastructure/Database/Sqlite/Queries.cs
+++ src/HuntBot.Infrastructure/Database/Sqlite/Queries.cs
@@ -4,5 +4,6 @@ namespace HuntBot.Infrastructure.Database.Sqlite
     {
         public const string GetAggregateById = "SELECT Id, StoredEvents FROM HuntBotGames WHERE Id = @AggregateId";
         public const string InsertNewHuntBotGame = "INSERT INTO HuntBotGames (Id, StoredEvents) VALUES(@Id, @StoredEvents) ON CONFLICT(Id) DO UPDATE SET StoredEvents=@StoredEvents";
+        public const string CheckIfAggregateExists = "SELECT COUNT(*) FROM HuntBotGames WHERE Id = @Id";
     }
 }
--- src/HuntBot.Infrastructure/EventSourcing/SqliteAggregateStore.cs
+++ src/HuntBot.Infrastructure/EventSourcing/SqliteAggregateStore.cs
@@ -14,20 +14,32 @@ namespace HuntBot.Infrastructure.EventStore
 {
     public class SqliteAggregateStore : IAggregateStore
     {
+        /// <summary>
+        /// SQLite connection factory with which connectivity is established.
+        /// </summary>
         private readonly SqliteConnectionFactory _sqliteConnectionFactory;
-
+        
+        /// <summary>
+        /// Initializes a new instance of <see cref="SqliteAggregateStore"/>.
+        /// </summary>
+        /// <param name="sqliteConnectionFactory">SQLite connection factory with which connectivity is established.</param>
         public SqliteAggregateStore(SqliteConnectionFactory sqliteConnectionFactory)
         {
             _sqliteConnectionFactory = sqliteConnectionFactory;
         }
-
+        
+        /// <summary>
+        /// Checks to see if an aggregate exists in the store.
+        /// </summary>
+        /// <param name="aggregateId">The Id of the aggregate.see cref=""/></param>
+        /// <typeparam name="T">The CLR type of the aggregate.</typeparam>
+        /// <returns>True if the aggregate is found in the store.</returns>
         public async Task<bool> Exists<T>(Guid aggregateId)
         {
             try
             {
                 var connection = _sqliteConnectionFactory.GetConnection(SqliteConnectionMode.Read);
-                var parameters = new { Id = aggregateId };
-                var count = await connection.ExecuteScalarAsync<int>("SELECT COUNT(*) FROM HuntBotGames WHERE Id = @Id", parameters);
+                var count = await connection.ExecuteScalarAsync<int>(Queries.CheckIfAggregateExists, new { Id = aggregateId });
 
                 return count == 1;
             }
@@ -39,6 +51,12 @@ namespace HuntBot.Infrastructure.EventStore
             return false;
         }
 
+        /// <summary>
+        /// Loads an aggregate record from the aggregate store.
+        /// </summary>
+        /// <param name="aggregateId">The Id of the aggregate being loaded.</param>
+        /// <typeparam name="T">The CLR type of the aggregate.</typeparam>
+        /// <returns>True if the aggregate is found in the store.</returns>
         public async Task<T> Load<T>(Guid aggregateId) where T : AggregateRoot
         {
             try
@@ -88,6 +106,11 @@ namespace HuntBot.Infrastructure.EventStore
             }
         }
 
+        /// <summary>
+        /// Stores a set of changes for a given aggregate record.
+        /// </summary>
+        /// <param name="aggregate">The aggregate record whose changes are being stored.</param>
+        /// <typeparam name="T">The CLR type of the aggregate.</typeparam>
         public async Task Save<T>(T aggregate) where T : AggregateRoot
         {
             List<StoredEvent> eventsToStore = new List<StoredEvent>();
@@ -152,7 +175,7 @@ namespace HuntBot.Infrastructure.EventStore
         /// <summary>
         /// Serializes an object to raw bytes.
         /// </summary>
-        /// <param name="data"></param>
+        /// <param name="data">To data that is being serialized.</param>
         /// <returns>The raw bytes of the serialized object.</returns>
         public static byte[] Serialize(object data)
         {
