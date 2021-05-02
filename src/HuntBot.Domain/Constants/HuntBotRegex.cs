3a43891057d1f0136a055595f4f75d3de01733fa
Brandon Avant <avant.brandon@gmail.com>
2021-05-02 00:07:43 -0500

    Adding clarification in XML documentation for Coordinates regex

--- src/HuntBot.Domain/Constants/HuntBotRegex.cs
+++ src/HuntBot.Domain/Constants/HuntBotRegex.cs
@@ -3,9 +3,9 @@
     public static class HuntBotRegex
     {
         /// <summary>
-        /// Regular expression used to parse the X, Y, Z and Yaw tokens out of a string. 
-        /// For example, given the string "120.44N 140.77E 0.5A 3599", the values
-        /// X = 140.77, Y = 0.5, Z = 120.44, and Yaw = 3599 will be parsed out.
+        /// Regular expression used to parse the World, X, Y, Z and Yaw tokens out of a string. 
+        /// For example, given the string "AW 120.44N 140.77E 0.5A 3599", the values
+        /// World = AW, X = 140.77, Y = 0.5, Z = 120.44, and Yaw = 3599 will be parsed out.
         /// </summary>
         /// TODO: This can be improved more to place limitation on decimal places and number ranges.
         public const string Coordinates = @"^(\w+){1}\s+(\d*\.?\d+[NS]){1}\s+(\d*\.?\d+[EW]){1}(\s+\d*\.?\d+[A])?(\s+\d+)?";
